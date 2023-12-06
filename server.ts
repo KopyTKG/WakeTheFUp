c7de8d2681b3f157717a03bcddd8266887d193be
Martin Kopecký <kopecky@thekrew.app>
2023-12-06 08:31:31 +0100

    base for backend

--- server.ts
+++ server.ts
@@ -1,2 +1,39 @@
+import * as ping from 'ping'
+import wol from 'wake_on_lan'
+import Express from 'express'
+import expressWs from 'express-ws'
+import { log } from 'console'
+const wss = expressWs(Express())
+const app = wss.app
 
-console.log('Hello from server.ts');
+app.get('/', (req, res) => {
+  res.sendStatus(200)
+})
+
+app.ws('/ws', (ws) => {
+  ws.on('message', (msg) => {
+    log('Received: %s', msg)
+    if (msg.toString() === 'wol') {
+      wol.wake('00:11:22:33:44:55', (err) => {
+        if (err) {
+          log(err)
+        } else {
+          log('WOL sent')
+        }
+      })
+    }
+  })
+
+  ws.on('close', () => {
+    log('Client disconnected')
+  })
+})
+
+app.listen(8080)
+
+ping.sys.probe('ujep.cz', (isAlive) => {
+  if (isAlive) {
+    console.log('Host is alive')
+  }
+})
+console.log('Hello from server.ts')
